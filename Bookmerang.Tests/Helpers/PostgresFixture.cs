deb31b10570cbb7a1d579908cbf52364d9f9c41c
FernandoTC18 <ftriguerocaballo@gmail.com>
2026-04-14 17:06:49 +0200

    feat(tutorial): persistir estado del tutorial en backend

--- Bookmerang.Tests/Helpers/PostgresFixture.cs
+++ Bookmerang.Tests/Helpers/PostgresFixture.cs
@@ -92,7 +92,8 @@ public class PostgresFixture : IAsyncLifetime
             "20260329120000_0008_add_inkdrops.sql",
             "20260405120000_0009_add_inkdrops_history.sql",
             "20260407000000_0008_bookdrop_exchange_status.sql",
-            "20260408120000_0008_chats_uuid_ids.sql"
+            "20260408120000_0008_chats_uuid_ids.sql",
+            "20260414113000_0010_add_tutorial_completed_to_users.sql"
         })
         {
             var sql = await File.ReadAllTextAsync(Path.Combine(migrationsDir, file));
